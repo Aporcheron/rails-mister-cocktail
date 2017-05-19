f4e97103a619e6e1277566bcec189a02e88de484
alban porcheron <alban.porcheron@gmail.com>
2017-05-19 16:23:09 +0200

    set_cocktail_image helper created

--- app/helpers/cocktails_helper.rb
+++ app/helpers/cocktails_helper.rb
@@ -1,2 +1,6 @@
 module CocktailsHelper
+  def set_cocktail_image(cocktail)
+    cocktail.photo.path.nil? ? url_image = image_path('mojito.jpeg') : url_image = cocktail.photo.path
+    return url_image
+  end
 end
