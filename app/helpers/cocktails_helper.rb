9f2517a08188fa41f32bf5318f6575b033bf1363
alban porcheron <alban.porcheron@gmail.com>
2017-05-19 17:08:06 +0200

    debugging image path

--- app/helpers/cocktails_helper.rb
+++ app/helpers/cocktails_helper.rb
@@ -1,5 +1,5 @@
 module CocktailsHelper
   def set_cocktail_image(cocktail)
-    cocktail.photo.nil? ? image_path('mojito.jpeg') : cl_image_path(cocktail.photo.path)
+    cocktail.photo.nil? ? asset_path('mojito.jpeg') : cl_image_path(cocktail.photo.path)
   end
 end
